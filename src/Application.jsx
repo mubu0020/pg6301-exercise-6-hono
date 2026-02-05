34364b7a10c3b4cc9c18d25a3be192a60f7775b0
mubu002 <mustafa_buga1@hotmail.com>
2026-02-05 20:19:19 +0100

    Lla inn funksjonaliteter som input mulighet av bruker, og legge til task muligheten og sende dette til serveren.

--- src/Application.jsx
+++ src/Application.jsx
@@ -2,10 +2,26 @@ import { useEffect, useState } from "react";
 
 export function Application() {
     const [tasks, setTasks] = useState([]);
+    const [description, setDescription] = useState("");
 
     async function loadTasks() {
         const res = await fetch("/api/tasks");
         setTasks(await res.json());
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!description.trim()) return;
+
+        await fetch("/api/tasks", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ description, completed: false }),
+        });
+
+        setDescription("");
+        loadTasks();
     }
 
     useEffect(() => {
@@ -15,6 +31,15 @@ export function Application() {
     return (
         <>
             <h1>My Task Manager</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <button>Add</button>
+            </form>
+
             <ul>
                 {tasks.map((t, index) => (
                     <li key={index}>{t.description}</li>
