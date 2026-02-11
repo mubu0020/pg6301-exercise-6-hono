267512f20d9926b96cacf737543916304340369f
mubu002 <mustafa_buga1@hotmail.com>
2026-02-11 13:26:41 +0100

    Serveren skal nå motta JSON-body, legge tasken i tasks-arrayet og svare med korrekt HTTP-status.

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ const tasks = [
     { description: "Create project (server)", completed: true },
     { description: "Create React webapp (server)", completed: true },
     { description: "Create Hono backend", completed: false },
-    { description: "Update with Hono backend", completed: false },
 ];
 
 app.get("/api/tasks", (c) => {
     return c.json(tasks);
+});
+
+app.post("/api/tasks", async (c) => {
+    const task = await c.req.json();
+    tasks.push(task);
+    return c.newResponse(null, 201);
 });
 
 serve(app);
